ec4e3608beec7b2d40ceb7d41fccc469b4066a85
Christopher-OOC <olojedechristopher24@gmail.com>
2024-09-13 18:56:51 +0100

    commit

---
+++ src/main/java/com/appsblogdeveloper/sociallogin/SocialLoginWebClient/security/WebSecurity.java
@@ -0,0 +1,30 @@
+package com.appsblogdeveloper.sociallogin.SocialLoginWebClient.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> {
+
+                });
+
+        return http.build();
+    }
+
+
+
+}
